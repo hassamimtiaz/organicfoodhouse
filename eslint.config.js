b0ffcf1b65bcd36ca3701ca44f71a35672c8ce0b
hassamimtiaz <hassam2522@gmail.com>
2026-06-28 19:43:48 +0500

    Fixed issueS

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,10 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
-import js from '@eslint/js'
-import reactHooks from 'eslint-plugin-react-hooks'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+
 const compat = new FlatCompat({ baseDirectory: __dirname })
 
-export default defineConfig([
-  globalIgnores(['.next', 'dist', 'node_modules']),
-  ...compat.extends('next/core-web-vitals'),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-    ],
-  },
-])
+export default [...compat.extends('next/core-web-vitals', 'next/typescript')]
